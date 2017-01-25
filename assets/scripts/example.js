6d792b0c713a90ab798bb224ebb61bf74eac2781
mario7746 <m.kennedykavouras@gmail.com>
2017-01-24 21:23:39 -0500

    Added makeMove and winCheck functions
    -updated styles and layout

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,3 +1,71 @@
 'use strict';
 
-module.exports = true;
+const gameBoard = {
+  0: null,
+  1: null,
+  2: null,
+  3: null,
+  4: null,
+  5: null,
+  6: null,
+  7: null,
+  8: null,
+};
+
+const winningRows = [
+  [0,1,2],
+  [2,5,8],
+  [6,7,8],
+  [0,3,5],
+  [6,4,2],
+  [0,4,8],
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+];
+
+const players = {
+  PLAYER_X: "Player X",
+  PLAYER_O: "Player O"
+};
+
+const makeMove = function (position, player){
+  gameBoard[position] = player;
+
+  if( checkWin(player) ){
+    console.log(player + 'has won!!!!!!!!!')
+  } else {
+    console.log('no one has one yet')
+  }
+
+};
+
+const checkWin = function (player) {
+  let playerHasWon = false;
+  for (let i = 0; i < winningRows.length; i++) {
+    let positionStore = [];
+    for (let y = 0; y < winningRows[i].length; y++) {
+      let currentPosition = winningRows[i][y]
+      if (gameBoard[currentPosition] === player){
+        positionStore.push(player);
+      }
+      if (positionStore.length > 1) {
+        playerHasWon = true
+      }
+    }
+  }
+  return playerHasWon;
+};
+
+
+module.exports = {
+  gameBoard,
+  winningRows,
+  players,
+  makeMove,
+  checkWin
+
+};
