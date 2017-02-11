0fa64c04d474a17bdd7ae930ae16c132b4391bcd
mario7746 <m.kennedykavouras@gmail.com>
2017-02-10 19:39:49 -0500

    added success and failiure functions to ui

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -6,81 +6,35 @@ const api = require('./api');
 const ui = require('./ui');
 const store = require('../store');
 
-const checkForUser = function() {
-  //if user is already signed in
-  if(!!store.user.id){
-    $('.btn-sign-in').hide();
-    $('.btn-create').show();
-    $('#sign-out').show();
-    $('.btn-change-pass').show();
-  } else {
-    $('#sign-out').hide();
-    $('.btn-change-pass').hide();
-    $('.btn-sign-in').show();
-  }
-};
-
 const onSignUp = function (event) {
   let data = getFormFields(event.target);
   event.preventDefault();
   api.signUp(data)
-    .then(ui.success)
-    .catch(ui.failure);
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure);
 };
 
 const onSignIn = function (event) {
+  $('.wrongCred').text('');
   let data = getFormFields(event.target);
   event.preventDefault();
   api.signIn(data)
-    .then((response) => {
-      store.user = response.user;
-      //keeps a copy of the user in local storage to keep  session open
-      window.localStorage.setItem('user', JSON.stringify(response.user));
-      $('#signIn').modal('hide');
-      $('.btn-create').show();
-      $('.btn-index').show();
-      $('.banner').show();
-      $('.showgames').show();
-
-
-      checkForUser();
-      return store.user;
-    })
-
-    .then(ui.success)
-    .catch(ui.failure);
-      $('.wrongCred').text("Wrong username or password! Try again");
+    .then(ui.signInsuccess)
+    .catch(ui.signInFailure);
 };
 
 const onChangePassword = function (event) {
   let data = getFormFields(event.target);
   event.preventDefault();
   api.changePassword(data)
-    .then(ui.success)
-    .catch(ui.failure);
-      // $('.wrongCredChange').text("Cannot be same password! Try again!");
+    .then(ui.passwordChangeSuccess)
+    .catch(ui.passwordChangeFailure);
 };
 
 const onSignOut = function (event) {
   event.preventDefault();
   api.signOut()
-    .then(() => {
-
-      $('.board').hide();
-      $('.btn-create').hide();
-      $('.btn-index').hide();
-      $('.banner').hide();
-      $('.showgames').hide();
-
-      store.user = {};
-      // remove local storage user copy.
-      window.localStorage.removeItem('user');
-      checkForUser();
-      return store;
-    })
-    .then(ui.success)
-    .catch(ui.failure)
-    ;
+    .then(ui.signOutSuccess)
 };
 
 
--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,11 +1,17 @@
 'use strict';
 
-const success = (data) => {
-  if (data) { console.log(data); }
-};
-
-const failure = (error) => {
-  console.error(error);
+const checkForUser = function() {
+  //if user is already signed in
+  if(!!store.user.id){
+    $('.btn-sign-in').hide();
+    $('.btn-create').show();
+    $('#sign-out').show();
+    $('.btn-change-pass').show();
+  } else {
+    $('#sign-out').hide();
+    $('.btn-change-pass').hide();
+    $('.btn-sign-in').show();
+  }
 };
 
 const changePasswordSuccess = () => {
@@ -14,8 +20,84 @@ const changePasswordSuccess = () => {
   $('#changePasswordModal').modal('hide');
 };
 
+
+const signUpFailure = (err) => {
+	if (err.status === 400) {
+		//unauthorized
+		$('.signUpMessage').text("A user with that email already exists.");
+	} else {
+		$('.signUpMessage').text("An unknown error occured.");
+	}
+};
+
+const signUpSuccess = (resp) => {
+	$('#signUp').modal('hide');
+	$('#sign-in input[name=credentials[email]').val(resp.email)
+};
+
+const signInSuccess = (resp) => {
+	store.user = resp.user;
+	//keeps a copy of the user in local storage to keep  session open
+	window.localStorage.setItem('user', JSON.stringify(resp.user));
+	$('#signIn').modal('hide');
+	$('.btn-create').show();
+	$('.btn-index').show();
+	$('.banner').show();
+	$('.showgames').show();
+
+
+	checkForUser();
+	return store.user;
+};
+
+const signOutSuccess = (resp) => {
+	$('.board').hide();
+	$('.btn-create').hide();
+	$('.btn-index').hide();
+	$('.banner').hide();
+	$('.showgames').hide();
+
+	store.user = {};
+	// remove local storage user copy.
+	window.localStorage.removeItem('user');
+	checkForUser();
+	return store;
+};
+
+const signInFailure = (err) => {
+	if (err.status === 401) {
+		//unauthorized
+		$('.wrongCred').text("Wrong username or password! Try again");
+	} else {
+		$('.wrongCred').text("An unknown error occured.");
+	}
+};
+
+const passwordChangeFailure = (err) => {
+	if (err.status === 400) {
+		//unauthorized
+		$('.password-message').text("Your existing password is incorect");
+	} else {
+		$('.password-message').text("An unknown error occured.");
+	}
+};
+
+const passwordChangeSuccess = (resp) => {
+	$('#changePassword').modal('hide');
+	$('.alert span').text('You have sucessfully changed your password!')
+	$('.alert').slideDown()
+
+	$('.alert').delay(2000).slideUp()
+};
+
+
 module.exports = {
-  failure,
-  success,
-  changePasswordSuccess
+  changePasswordSuccess,
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  passwordChangeFailure,
+  passwordChangeSuccess,
+  signOutSuccess
 };
